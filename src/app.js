84722b0829e3f64a7f9afe32bc4dfb2858fc2422
flaviamartins29 <69690914+flaviamartins29@users.noreply.github.com>
2020-09-04 07:56:23 -0300

    Aggregate repo routes using on router

--- src/app.js
+++ src/app.js
@@ -4,12 +4,15 @@ const db = require('./inMemoryDB')
 
 const { repositoryNotFoundError, created, success, noContent } = require('./utils')
 
-// const { Router } = express
+const { Router } = express
+
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.param(':id', async (req, res, next, id) => {
+const repoRouter = new Router()
+
+repoRouter.param(':id', async (req, res, next, id) => {
   const repository = await db.getById(id)
 
   if (!repository) {
@@ -20,37 +23,39 @@ app.param(':id', async (req, res, next, id) => {
   next()
 })
 
-app.get('/repositories', async ({ body }, res) => {
+repoRouter.get('/', async ({ body }, res) => {
   const results = await db.list(body.title)
 
   success(res, results)
 })
 
-app.post('/repositories', async ({ body }, res) => {
+repoRouter.post('/', async ({ body }, res) => {
   const repository = await db.create(body)
 
   created(res, repository)
 })
 
-app.put('/repositories/:id', async ({ body, params }, res) => {
+repoRouter.put('/:id', async ({ body, params }, res) => {
   const { title, techs, url } = body
   const repository = await db.update(params.id, { title, techs, url })
 
   success(res, repository)
 })
 
-app.delete('/repositories/:id', async ({ params }, res) => {
+repoRouter.delete('/:id', async ({ params }, res) => {
   await db.remove(params.id)
 
   noContent(res)
 })
 
-app.post('/repositories/:id/likes', async ({ params, repository }, res) => {
+repoRouter.post('/:id/likes', async ({ params, repository }, res) => {
   const updatedRepository = await db.update(params.id, {
     likes: (repository.likes += 1),
   })
 
   created(res, updatedRepository)
 })
+
+app.use('/repositories', repoRouter)
 
 module.exports = app
