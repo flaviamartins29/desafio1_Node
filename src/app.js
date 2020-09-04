cb3c57bd3a6193eb4505c860b4dbb829f5f1eed1
flaviamartins29 <69690914+flaviamartins29@users.noreply.github.com>
2020-09-04 08:32:20 -0300

    Inject  DB dependency into repor routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const cors = require('cors')
-const repoRouter = require('./routes/repositories')
+const db = require('./inMemoryDB')
+const initRepoRouter = require('./routes/repositories')
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use('/repositories', repoRouter)
+app.use('/repositories', initRepoRouter(db))
 
 module.exports = app
