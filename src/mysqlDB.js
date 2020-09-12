ce0daed3ed6ab63836bb609265b93179952038ee
flaviamartins29 <69690914+flaviamartins29@users.noreply.github.com>
2020-09-12 14:59:24 -0300

    Prepare mysql db functions

--- src/mysqlDB.js
+++ src/mysqlDB.js
@@ -12,7 +12,33 @@ async function initDb() {
 
   await db.query('SELECT 1 + 1 as OK')
 
-  return db
+  async function create(repository) {
+    // await db.query('')
+  }
+
+  async function update(id, repository) {}
+
+  async function remove(id) {}
+
+  async function list(title) {}
+
+  async function getById(id) {}
+
+  return { getById, list, remove, update, create }
 }
 
 module.exports = initDb
+
+/*
+    select * from repositories
+
+    select * from repositories where title like "%renato%"
+
+    select * from repositories where id=3
+
+    insert into repositories (title, url) values ("Repo do Renato", "www.repo.com")
+
+    update repositories set title="Novo Repo" where id=1
+
+    update repositories set likes=likes+1 where id=1
+*/
